5dea869bba348b04d1d7d347f7fd066201abdc91
Mamun Salem <salemtwin2@gmail.com>
2024-10-19 19:05:05 -0400

    Create Section class to manage books and availability

--- library_management.js
+++ library_management.js
@@ -22,4 +22,34 @@ class Book {
         this._isAvailable = status;
       }
     }
-    
+
+    //Task 2- Creating a section Class
+    class Section {
+        constructor(name) {
+          this.name = name;
+          this.books = []; // Array to store books in the section
+        }
+      
+        // Tells use when a new book has beed added
+        addBook(book) {
+          this.books.push(book);
+          console.log(`New addition to the ${this.name} section: "${book.title}" by ${book.author} is now available!`);
+        }
+      
+        // calculates how many available books we have  
+        getAvailableBooks() {
+          return this.books.filter(book => book.isAvailable).length;
+        }
+      
+        // listing  all books and then their availability
+        listBooks() {
+          console.log(`Books currently in the ${this.name} section:`);
+          this.books.forEach(book => {
+            if (book.isAvailable) {
+              console.log(`"${book.title}" by ${book.author} is available for borrowing.`);
+            } else {
+              console.log(`"${book.title}" by ${book.author} is currently borrowed. Please check back later.`);
+            }
+          });
+        }
+      }
